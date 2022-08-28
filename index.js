11f3058fcdbbac4d619f019264fd98cabb063ee0
joyreuben <67860973+joyreuben@users.noreply.github.com>
2022-08-28 15:58:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function openNav(x) {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("arrow").classList.toggle("caret-rotate");
+
+}
+
+function openNavs(x) {
+    document.getElementById("drop2").classList.toggle("show");
+    document.getElementById("arrow2").classList.toggle("caret-rotate");
+}
+
+function sideMenu(x) {
+    document.getElementById("ham-menu").classList.toggle("show");
+    
+}
+
+function closeMenu(x) {
+    document.getElementById("ham-menu").classList.toggle("hide");
+    document.getElementById("ham-menu").classList.remove("hide");
+}
+
